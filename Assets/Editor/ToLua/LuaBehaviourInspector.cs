2f0d9271957fbd94aceda4f26d6b3b7369ef62f7
qintianchen <1365265750@qq.com>
2022-01-14 20:33:01 +0800

    feat: LuaBehaviour for binding UnityGameObject to lua

---
+++ Assets/Editor/ToLua/LuaBehaviourInspector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LuaBehaviourInspector))]
+public class LuaBehaviourInspector: Editor
+{
+    private Vector2 scrollPos;
+    private LuaBindElement.BindType bindType = LuaBindElement.BindType.GameObject;
+    
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var behaviour = target as LuaBehaviour;
+        
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.BeginHorizontal(GUILayout.MaxHeight(32));
+        bindType = (LuaBindElement.BindType) EditorGUILayout.EnumPopup(bindType, GUILayout.MaxHeight(32)); 
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndScrollView();
+    }
+}
---
+++ Assets/Scripts/ToLua/LuaBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+[System.Serializable]
+public class LuaBindElement
+{
+    public enum BindType
+    {
+        GameObject,
+        Transform,
+        RectTransform,
+        Image,
+        Text,
+        Button,
+        ScrollView
+    }
+    
+    public BindType type;
+    public Object obj;
+    public string luaName;
+}
+
+public class LuaBehaviour : MonoBehaviour
+{
+    public string LuaFileName;
+    [HideInInspector] public List<LuaBindElement> elements;
+}
